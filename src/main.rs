a8ee45475a3985a6b3a7dff71d17518f1beee0a7
dunlopWill <willmdunlop@gmail.com>
2025-08-28 23:01:59 +0100

    get_number_doos fn more readable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
+use std::num::ParseIntError;
 
-fn get_number_of_doos() -> u8 {
+fn get_cmd_line_input() -> ArgMatches<'static> {
     let matches = App::new("bbshark")
         .version("0.1.2")
         .author("https://github.com/dunlopWill")
@@ -16,9 +17,10 @@ fn get_number_of_doos() -> u8 {
                 .max_values(1)
         )
         .get_matches();
-    let binding = matches.values_of_lossy("doos").unwrap();
-    let doos = binding.first().unwrap();
-    let number_result = doos.parse::<u8>();
+    return matches
+    }
+
+fn check_is_u8(number_result: Result<u8, ParseIntError>, doos: &String) -> u8 {
     let number = match number_result {
         Ok(num) => num,
         Err(_error) => {
@@ -26,11 +28,24 @@ fn get_number_of_doos() -> u8 {
             std::process::exit(1)  // Exit with a non-zero status to indicate error
         },
     };
+    return number
+}
+
+fn check_number_is_not_zero(number:u8, doos: &String) -> u8 {
     if number == 0 {
         eprintln!("Must enter an unsigned integer between 1 and 127 (u8). Got '{}'", doos);
         std::process::exit(1)  // Exit with a non-zero status to indicate error
     };
     return number
+} 
+
+fn get_number_of_doos() -> u8 {
+    let matches = get_cmd_line_input();
+    let binding = matches.values_of_lossy("doos").unwrap();
+    let doos = binding.first().unwrap();
+    let number_result = doos.parse::<u8>();
+    let number = check_is_u8(number_result, doos);
+    return check_number_is_not_zero(number, doos)
 }
 
 fn print_doos_times(number: u8) {
